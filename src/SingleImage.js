3bd32f4503e3847da876d4c16a9da13433546110
Brennen <ketron@outlook.com>
2017-03-31 13:06:49 -0700

    finished the adventure mode

--- src/SingleImage.js
+++ src/SingleImage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 var albums = require('./albums')
 
@@ -38,6 +39,39 @@ var styles = {
 		flexDirection: 'column',
 		justifyContent: 'center'
 	},
+	navButtons: {
+		height: '100%',
+		width: '75%',
+		display: 'flex',
+		justifyContent: 'space-between',
+		margin: 'auto',
+		fontSize: '30px',
+		color: 'white',
+		position: 'relative',
+		top: '175px',
+		padding: '0 10px'
+	},
+	navLeft: {
+		width: '40px',
+		height: '40px',
+		textAlign: 'center',
+		lineHeight: '40px',
+		borderRadius: '10px',
+		paddingRight: '5px',
+		marginLeft: '15px'
+	},
+	navRight: {
+		width: '40px',
+		height: '40px',
+		textAlign: 'center',
+		lineHeight: '40px',
+		borderRadius: '10px',
+		paddingLeft: '5px',
+		marginRight: '15px'
+	},
+	navIconRight: {
+		marginLeft: '2px'
+	}
 }
 
 export default React.createClass({
@@ -57,15 +91,56 @@ export default React.createClass({
 		e.preventDefault()
 		this.props.history.goBack()
 	},
+	nextPic() {
+		var picLink = '/album/' + this.state.currentIndex + '/photos/'
+		var numPics = albums[(this.props.match.params.albumIndex-1)].Photos.length
+		var currentPic = Number(this.props.match.params.photoId)
+		var nextPic = 0
+
+		if( currentPic === numPics ) {
+			nextPic = 1
+		} else {
+			nextPic = currentPic + 1
+		}
+
+		picLink = picLink + nextPic
+
+		return picLink
+	},
+	prevPic() {
+		var picLink = '/album/' + this.state.currentIndex + '/photos/'
+		var numPics = albums[(this.props.match.params.albumIndex-1)].Photos.length
+		var currentPic = Number(this.props.match.params.photoId)
+		var nextPic = currentPic -1
+
+		if( nextPic === 0 ) {
+			nextPic = 6
+		} else {
+			nextPic = currentPic - 1
+		}
+
+		picLink = picLink + nextPic
+
+		return picLink
+
+	},
 	render() {
 		return (
 			<div style={styles.main}>
-				<button style={styles.backButton} onClick={this.handleBack} type="button"><i className="fa fa-chevron-left" style={styles.icon} aria-hidden="true"></i> Album {this.props.match.params.albumIndex}</button>
+				{console.log(this.props.match.params.photoId)}
+				<button style={styles.backButton} onClick={this.handleBack} type="button"><i style={styles.navIconRight} className="fa fa-chevron-left" style={styles.icon} aria-hidden="true"></i> Album {this.props.match.params.albumIndex}</button>
+
+				<div style={styles.navButtons} className="navButtons">
+					<Link style={styles.navLeft} to={this.prevPic()}><i className="fa fa-caret-left" aria-hidden="true"></i></Link>
+					<Link style={styles.navRight} to={this.nextPic()}><i className="fa fa-caret-right" aria-hidden="true"></i></Link>
+				</div>
 
 				<div style={styles.container}>
 					<p style={styles.header}>Photo {this.props.match.params.photoId}</p>
 					<img style={styles.mainPic} src={require(albums[(this.props.match.params.albumIndex-1)].Photos[(this.props.match.params.photoId-1)].link)} />
 				</div>
+
+				
 
 				{console.log(albums[(this.props.match.params.albumIndex-1)].Photos[(this.props.match.params.photoId-1)].link)}
 			</div>
